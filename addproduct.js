1addadaafae2ae86e7116d8b3d5ad9a5392be3bf
vignesh222002 <gtavignesh22042002@gmail.com>
2024-02-03 10:47:22 +0530

    Vignesh Changes

--- addproduct.js
+++ addproduct.js
@@ -1,11 +1,28 @@
+let gender = '';
+let colors = [];
+
 const product = document.getElementById("product");
 const Description = document.getElementById("description");
 const Brand = document.getElementById("brand");
 const Quantity = document.getElementById("quantity");
-const Gender = document.getElementById("gender");
 const Colors = document.getElementById("colors");
 const Category = document.getElementById("category");
 const button = document.getElementById("butn");
+
+const Gender1 = document.getElementById("Gender1");
+const Gender2 = document.getElementById("Gender2");
+const Gender3 = document.getElementById("Gender3");
+
+Gender1.addEventListener('click', (e) => {
+  gender = e.target.value
+})
+Gender2.addEventListener('click', (e) => {
+  gender = e.target.value
+})
+Gender3.addEventListener('click', (e) => {
+  gender = e.target.value
+})
+
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
@@ -15,11 +32,12 @@ button.addEventListener("click", (e) => {
     description: Description.value,
     brand: Brand.value,
     quantity: Quantity.value,
-    gender:Gender.value,
+    gender: gender,
   };
-  // console.log(userdetails.description);
   localStorage.setItem(userdetails.id, JSON.stringify(userdetails));
-  console.log(userdetails);
+  window.location.reload();
+  // console.log(userdetails);
+  // console.log(userdetails.description);
   // alert("user create successfully");
 });
 
